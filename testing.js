6e44c272fac56e8ccfb45ae0cb44786f622f6eba
danghoa1 <danghoaiphi@Phis-MacBook-Air.local>
2013-09-03 23:06:03 -0700

    changed testing.js

--- testing.js
+++ testing.js
@@ -6,6 +6,11 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/danghoa1', function(request, response) {
+  response.send('Hello danghoa1!');
+});
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
